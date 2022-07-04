a21fc72846cba031d4033c3e40292b5528fc6ddc
Fernando Verly <fverly@ad.finep.gov.br>
2022-07-04 17:53:59 -0300

    Componente do tipo hello world criado

---
+++ src/components/TabelaFii.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>Fii's</header>
+    <div>FUNCIONOU!!!</div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue'
+
+    export default defineComponent({
+        name: 'TabelaFii'
+    })
+</script>
+
+<style scoped>
+</style>
