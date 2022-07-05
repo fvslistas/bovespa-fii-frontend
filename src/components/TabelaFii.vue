97aa11a38c8c9657cc3a8b55d6d13ebf16752d94
Fernando Verly <fverly@ad.finep.gov.br>
2022-07-05 14:18:25 -0300

    Acessando api java em rest usando axios

--- src/components/TabelaFii.vue
+++ src/components/TabelaFii.vue
@@ -1,13 +1,48 @@
 <template>
-    <header>Fii's</header>
-    <div>FUNCIONOU!!!</div>
+    <div>
+        <header>Fii's</header>
+        <table>
+            <thead>
+                <tr>
+                    <th>Razão</th>
+                    <th>Fundo</th>
+                    <th>Segmento</th>
+                    <th>Código</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="fii in fiis" :key="fii.razao">
+                    <td>{{fii.razao}} </td>
+                    <td>{{fii.fundo}}</td>
+                    <td>{{fii.segmento}}</td>
+                    <td>{{fii.codigo}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script lang="ts">
+    import axios from 'axios'
     import { defineComponent } from 'vue'
 
     export default defineComponent({
-        name: 'TabelaFii'
+        name: 'TabelaFii',
+        data() {
+            return {
+                fiis: {},
+            }
+        },
+        mounted () {
+            axios
+            .get('http://localhost:8081/fii/list')
+            .then((res)=>{
+                this.fiis = res.data;
+            })
+            .catch((error)=>{
+                console.log(error);
+            })
+        }
     })
 </script>
 
