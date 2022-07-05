621ad626bab6e794ea3de399cc2bd5b07eaa01f3
Fernando Verly <fverly@ad.finep.gov.br>
2022-07-05 17:24:19 -0300

    Docker

--- docker cria imagem e inicia.sh
+++ docker cria imagem e inicia.sh
@@ -1,2 +1,4 @@
-docker build -t fvslistas/bovespa-fii-frontend:0.0.1 .
-docker run -d -p 8080:80 --rm --name bovespa-fii-frontend fvslistas/bovespa-fii-frontend:0.0.1  
+#!/bin/sh
+docker rm -f bovespa-fii-frontend
+docker build -t fvslistas/bovespa-fii-frontend:0.0.2 .
+docker run -d -p 8080:80 --network bovespa --rm --name bovespa-fii-frontend fvslistas/bovespa-fii-frontend:0.0.2
